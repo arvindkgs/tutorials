8da5963e847764ba9e709b7166099d08327da3c9
arvindkgs <arvindkgs@nile-global.com>
2021-07-22 14:25:49 +0530

    Add controller

---
+++ patterns/hexagonal-architecture-arvindkgs/hexagonal/src/main/java/com/baeldung/hexagonal/controller/BillingController.java
@@ -0,0 +1,24 @@
+package com.baeldung.hexagonal.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.baeldung.hexagonal.domain.BillingHandler;
+import com.baeldung.hexagonal.domain.Order;
+import com.baeldung.hexagonal.exception.PaymentException;
+import com.stripe.exception.StripeException;
+
+public class BillingController {
+
+    private final BillingHandler billingHandler;
+
+    public BillingController(BillingHandler billingHandler) {
+        this.billingHandler = billingHandler;
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> pay(@RequestBody Order order) throws PaymentException, StripeException {
+        return ResponseEntity.ok(billingHandler.pay(order));
+    }
+}
